ad43438c7accbb6c3b4d8e13cd79bb9bdda2aa87
network-Error <iamimew@gmail.com>
2022-03-02 19:28:29 +0300

    modified App.js, Mugs.js, style.scss; add image to assets;

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Mugs from './components/Mugs/Mugs';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
+        <Mugs />
       </div>
     </BrowserRouter>
   );
---
+++ src/components/Mugs/Mugs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../style.scss';
+
+const Mugs = () => {
+    return (
+        <div className='mugs'>
+            <div className='mugs__text'>
+                <span>Best place to buy design</span>
+                <h1>Coffee Mugs</h1>
+                <p>The most versatile furniture system ever created. Designed to fit your life, made to move and grow.</p>
+                <button>Explore Our Products</button>
+            </div>
+        </div>
+    )
+}
+
+export default Mugs;
