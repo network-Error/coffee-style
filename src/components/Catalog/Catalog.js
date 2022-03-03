16602b1a67c8f13de6f57b9742a42b4d5686a4ea
network-Error <iamimew@gmail.com>
2022-03-03 20:08:16 +0300

    modified: Catalog.js, style.scss; add: images

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -2,6 +2,18 @@ import React from 'react';
 import CardShop from './card';
 import pinkPremium from '../../assets/mugs1.png';
 import goldenMugs from '../../assets/mugs2.png';
+import product1 from '../../assets/product1.jpg';
+import product2 from '../../assets/product2.jpg';
+import product3 from '../../assets/product3.jpg';
+import product4 from '../../assets/product4.jpg';
+import product5 from '../../assets/product5.jpg';
+import product6 from '../../assets/product6.jpg';
+import product7 from '../../assets/product7.jpg';
+import product8 from '../../assets/product8.jpg';
+import product9 from '../../assets/product9.jpg';
+import offer1 from '../../assets/offer1.jpg';
+import offer2 from '../../assets/offer2.jpg';
+import offer3 from '../../assets/offer3.jpg';
 import '../style.scss';
 
 const Catalog = (props) => {
@@ -23,6 +35,53 @@ const Catalog = (props) => {
                 <div className='catalog__featured__items'>
                     <CardShop img={pinkPremium} alt={'pinkPremium'} text={'Pink Premium Ceramic'} price={'$ 99.00 USD'} />
                     <CardShop className={'cardShop__price cardShop__innv'} img={goldenMugs} alt={'goldenMugs'} text={'Golden Designers Mug'} prices={'$50.00'} price={'$ 69.00 USD'} />
+                </div>
+
+            </div>
+
+            <div className='catalog__products'>
+                <div className='catalog__products__header'>
+                    <span></span>
+                    <p>More Products</p>
+                    <span></span>
+                </div>
+
+                <div className='catalog__products__items'>
+                    <CardShop className={'cardShop__price cardShop__innv'} img={product1} alt={'product'} text={'Red Love Cap'} prices={'$25.00'} price={'$37.00 USD'} />
+                    <CardShop className={'cardShop__price cardShop__innv'} img={product2} alt={'product'} text={'Black Tea Cup'} prices={'$15.00'} price={'$29.00 USD'} />
+                    <CardShop img={product3} alt={'product'} text={'B&W Essentials Mug'} price={'$19.00 USD'} />
+                    <CardShop img={product4} alt={'product'} text={'Winter Style Mug'} price={'$25.00 USD'} />
+                    <CardShop img={product5} alt={'product'} text={'Ceramic Tea'} price={'$46.00 USD'} />
+                    <CardShop img={product6} alt={'product'} text={'No Handle Bar Cup'} price={'$34.00 USD'} />
+                    <CardShop img={product7} alt={'product'} text={'Espresso Cup by Mugs.co'} price={'$25.00 USD'} />
+                    <CardShop img={product8} alt={'product'} text={'Pink Premium Ceramic'} price={'$99.00 USD'} />
+                    <CardShop img={product9} alt={'product'} text={'Summer Designer Cup'} price={'$29.00 USD'} />
+                </div>
+            </div>
+
+            <div className='catalog__magazine'>
+                <div className='catalog__magazine__header'>
+                    <span></span>
+                    <p>Buy 2 mugs and get a coffee magazine free</p>
+                    <span></span>
+                </div>
+
+                <div className='catalog__magazine__items'>
+                    <div className='catalog__magazine__items__img'>
+                        <div>
+                            <img src={offer1} alt='Offer' />
+                        </div>
+                        <div className='img-wrapper'>   
+                            <img src={offer2} alt='Offer' />
+                            <img src={offer3} alt='Offer' />
+                        </div>
+                    </div>
+                    <div className='catalog__magazine__items__text'>
+                        <span>Premium Offer</span>
+                        <h3>Get our Coffee Magazine</h3>
+                        <p>The most versatile furniture system ever created. Designed to fit your life.</p>
+                        <button>Start Shopping</button>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ src/components/Catalog/productCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style.scss';
+
+const productCard = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default productCard;
