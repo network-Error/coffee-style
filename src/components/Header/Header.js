195226acf8b4ceb6e1203c88f332a3b5caa3fd6b
network-Error <iamimew@gmail.com>
2022-03-08 00:29:52 +0300

    add adaptive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -23,6 +23,22 @@ const Header = () => {
                 <Link to='#'><img src={basket} alt='Basket' />Cart</Link>
                 <img src={basketCount} alt='Basket count' />
             </div>
+
+            <div className="hamburger-menu">
+                <input id="menu__toggle" type="checkbox" />
+                <label className="menu__btn" htmlFor="menu__toggle">
+                <span></span>
+                </label>
+
+                <ul className="menu__box">
+                    <li><Link className="menu__item" to="#">Home</Link></li>
+                    <li><Link className="menu__item" to="#">Our Products</Link></li>
+                    <li><Link className="menu__item" to="#">Blog</Link></li>
+                    <li><Link className="menu__item" to="#">About</Link></li>
+                    <li><Link className="menu__item" to="#">Contact</Link></li>
+                    <li><Link className="menu__item" to="#">Styleguide</Link></li>
+                </ul>
+            </div>
         </nav>
     )
 }
